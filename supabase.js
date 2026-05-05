30748bbc16d6bfe80aa122e2bb5d4fa62f1f1e8a
Alalani-school <alalani@bayviewglen.ca>
2026-05-05 09:30:15 -0400

    Update and rename supabase.js to index.html

--- supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabase = createClient(
-  'YOUR_URL',
-  'YOUR_ANON_KEY'
-)
-
-export default supabase
